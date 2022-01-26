5fdefe5c31eff9d43c1a3aae14f4763d6c8b9576
Hein <Hein@DESKTOP-8BJS9VC>
2022-01-26 12:45:17 -0800

    corrected the page count

--- Mandalay/Controllers/ProductController.cs
+++ Mandalay/Controllers/ProductController.cs
@@ -26,7 +26,9 @@ namespace Mandalay.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null?
+                        repository.Products.Count():
+                        repository.Products.Where(e => e.Category == category).Count()
                 },
                 CurrentCategory = category
             });
