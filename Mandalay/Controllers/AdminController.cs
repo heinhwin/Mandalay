972b0a0dc01fa7d33e2d7daa86e85f6798bf536b
Hein <Hein@DESKTOP-8BJS9VC>
2022-02-08 17:17:25 -0800

    added create and delete product action method

--- Mandalay/Controllers/AdminController.cs
+++ Mandalay/Controllers/AdminController.cs
@@ -27,9 +27,20 @@ namespace Mandalay.Controllers
             }
             else
             {
-                //there is something wrong with the data values
                 return View(product);
             }
+        }
+        public ViewResult Create() => View("Edit", new Product());
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if(deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+            return RedirectToAction("Index");
         }
     }
 }
